b97e764335650b67927e4d91754fa5ddeff86491
antoninaorobets <pani.antonina.don@gmail.com>
2021-11-22 15:16:45 -0500

    commit tree 5e3e9c4d2ee6642365bc4c2f4831c005d8a39ecf

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
-function scuberGreetingForFeet(){
+let reply;
+
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if (ride <= 400) {
+   reply = "This one is on me!";
+    }
+        else if ((ride > 2000) && (ride < 2500)) {
+          reply = 'I will gladly take your thirty bucks.'
+        }
+            else if (ride > 2500) {
+              reply ='No can do.'
+            }
+return reply;
+  } 
+
+let replyCity;
+
+function ternaryCheckCity(city){
+  // Write your code here!
+  // if (city === 'NYC') {
+  //   replyCity ="Ok, sounds good.";
+  // }
+  //else replyCity = "No go.";
+   switch (city) {
+     case 'NYC': 
+          replyCity ="Ok, sounds good."
+          break;
+     default: replyCity = "No go."
+   }
+  return replyCity;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+let anser;
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(rep){
   // Write your code here!
-}
+  switch (rep) {
+    case 'generous': 
+      anser = "Thank you so much.";
+      break;
+    case 'not as generous': 
+      anser = "Thank you.";
+      break;
+    default: anser = "Bye.";
+
+  }
+ return anser;
+ }
